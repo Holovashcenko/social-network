1985bf8ff173184f419b9cb944cdeff6186aca25
Vlad Holovashchenko <holovashcenko@gmail.com>
2020-05-13 17:08:00 +0300

    There's my first commit. Added Header, Navbar, Profile

---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Header = props => <header className="header"><img src="https://avatarko.ru/img/kartinka/1/multfilm_gomer.png" alt="logo" /></header>
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Profile = props =>
+    <div className="content">
+        <div><img width="100%" height="200px" src="https://jssors8.azureedge.net/demos/image-slider/img/faded-monaco-scenery-evening-dark-picjumbo-com-image.jpg" alt="logo" /></div>
+        <div>
+            <div><img height="100px" src="https://avatarko.ru/img/kartinka/1/multfilm_gomer.png" alt="avatar" /></div>
+            <div>description</div>
+        </div>
+        <div>
+            My posts
+            <div>new post</div>
+            <div>
+                <div>post1</div>
+                <div>post3</div>
+            </div>
+        </div>
+    </div>
+
+export default Profile;
