63b91d1f6481e7e8487a903f1196bf9f154da468
Vlad Holovashchenko <holovashcenko@gmail.com>
2020-05-18 21:11:58 +0300

    Added redux, created reduxStore, changed profileReducer and dialogsReducer, took off the store.js

--- src/redux/dialogsReducer.js
+++ src/redux/dialogsReducer.js
@@ -1,7 +1,45 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 const UPDATE_NEW_MESSAGE_TEXT = "UPDATE-NEW-MESSAGE-TEXT";
 
-const dialogsReducer = (state, action) => {
+let intialState = {
+  dialogsData: [
+    {
+      id: 1,
+      name: "Vlad",
+      img: "https://avatarko.ru/img/kartinka/1/multfilm_gomer.png",
+    },
+    {
+      id: 2,
+      name: "Alla",
+      img: "https://avatarko.ru/img/kartinka/1/multfilm_gomer.png",
+    },
+    {
+      id: 3,
+      name: "Anyta",
+      img: "https://avatarko.ru/img/kartinka/1/multfilm_gomer.png",
+    },
+    {
+      id: 4,
+      name: "Archy",
+      img: "https://avatarko.ru/img/kartinka/1/multfilm_gomer.png",
+    },
+  ],
+  messagesData: [
+    { id: 1, message: "Hello" },
+    { id: 2, message: "How are you?" },
+    { id: 3, message: "Fine. Thanks! And you?" },
+    { id: 4, message: "I'm fine, too. Thank you!" },
+    { id: 5, message: "I'm fine, too. Thank you!" },
+    { id: 6, message: "I'm fine, too. Thank you!" },
+    { id: 7, message: "I'm fine, too. Thank you!" },
+    { id: 8, message: "I'm fine, too. Thank you!" },
+    { id: 9, message: "I'm fine, too. Thank you!" },
+    { id: 10, message: "I'm fine, too. Thank you!" },
+  ],
+  newMessageText: "",
+};
+
+const dialogsReducer = (state = intialState, action) => {
   switch (action.type) {
     case ADD_MESSAGE:
       let newMessage = { id: 11, message: state.newMessageText };
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,7 +1,15 @@
 const ADD_POST = "ADD-POST";
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const profileReducer = (state, action) => {
+let intialState ={
+  postData: [
+    { id: 1, message: "Hi! How are you?", counterLikes: 23 },
+    { id: 2, message: "It's my first post!", counterLikes: 0 },
+  ],
+  newPostText: "",
+};
+
+const profileReducer = (state = intialState, action) => {
   switch (action.type) {
     case ADD_POST:
       let newPost = { id: 3, message: state.newPostText, counterLikes: 0 };
