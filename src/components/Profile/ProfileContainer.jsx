d90e415c8a8d8bf9076bb30fac93f0fbb8def57d
Vlad Holovashchenko <holovashcenko@gmail.com>
2020-05-23 20:36:36 +0300

    Added with router to profile container

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,15 @@ import Profile from "./Profile";
 import { connect } from "react-redux";
 import * as Axios from "axios";
 import { setUserProfile } from "../../redux/profileReducer";
+import { withRouter } from "react-router-dom";
 
 class ProfileContainer extends React.Component {
-
   componentDidMount() {
-    Axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+    let userId = this.props.match.params.userId;
+    if (!userId) {
+      userId = 2;
+    }
+    Axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
       .then(response => {
         this.props.setUserProfile(response.data);
       });
@@ -24,5 +28,7 @@ let mapStateToProps = (state) => {
   return{profile: state.profile.profile}
 }
 
+let WithUrlDataContainerComponent = withRouter(ProfileContainer);
 
-export default connect(mapStateToProps, { setUserProfile })(ProfileContainer)
+
+export default connect(mapStateToProps, { setUserProfile })(WithUrlDataContainerComponent)
