b95e427029c9dbceae617fdf0e2d0a97953b3b04
Vlad Holovashchenko <holovashcenko@gmail.com>
2020-05-23 12:03:52 +0300

    Made a functional component Users.jsx and classes conponent UsersContainer and pulled its to UsersContainer.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,38 @@
+import React from "react";
 import { connect } from "react-redux";
-import Users from "./Users";
 import { followAC, unfollowAC, setUsersAC, setCurrentPageAC, setTotalUsersCountAC } from "../../redux/usersReducer";
+import Users from "./Users";
+import * as Axios from "axios";
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        Axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
+    render() {
+        return <Users
+            users={this.props.users}
+            totalUserCount={this.props.totalUserCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            unfollow={this.props.unfollow}
+            follow={this.props.follow}
+        />
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -21,6 +53,4 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
