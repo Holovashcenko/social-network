baff028146573840238cadf08608cd14d54acc79
Vlad Holovashchenko <holovashcenko@gmail.com>
2020-05-26 22:35:11 +0300

    Created Login.jsx

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Route } from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import Login from "./components/Login/Login";
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
           <Route path="/music" render={() => <Music />} />
           <Route path="/users" render={() => <UsersContainer />} />
           <Route path="/settings" render={() => <Settings />} />
+          <Route path="/login" render={() => <Login />} />
         </div>
       </div>
   );
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from "./Login.module.css"
+
+const Login = (props) => {
+    return (
+        <div class={s.login}><h1>Login</h1></div>
+    )
+}
+
+export default Login;
