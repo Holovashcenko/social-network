fbee74542f6dae2679d11ec902c931d7adc8e718
Vlad Holovashchenko <holovashcenko@gmail.com>
2020-06-01 22:25:11 +0300

    Changed App to class component

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 
-const App = () => {
-  return (
+class App extends React.Component {
+  render() {
+    return (
       <div className="app-wrapper">
         <HeaderContainer />
         <Navbar />
         <div className="app-wrapper-content">
-          <Route path="/dialogs" render={() => <DialogsContainer /> } />
-          <Route path="/profile/:userId?" render={() => <ProfileContainer /> } />
+          <Route path="/dialogs" render={() => <DialogsContainer />} />
+          <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
           <Route path="/news" render={() => <News />} />
           <Route path="/music" render={() => <Music />} />
           <Route path="/users" render={() => <UsersContainer />} />
@@ -26,7 +27,8 @@ const App = () => {
           <Route path="/login" render={() => <Login />} />
         </div>
       </div>
-  );
+    );
+  }
 };
 
 export default App;
